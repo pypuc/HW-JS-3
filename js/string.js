42a4a548ba800b555a6cc8e7b41ea9acbd7e8729
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2024-10-04 16:00:42 +0300

    home work

---
+++ js/string.js
@@ -0,0 +1,27 @@
+// завдання 1
+
+const number = 5+5;
+const stingNumber = "5";
+const result = number + stingNumber;
+console.log(typeof result)
+
+// завдання 2
+
+const email = "rheghergh@ds.com";
+console.log(email.length)
+console.log(email.includes("@"));
+
+// завдання 3
+
+const myName = "My";
+const nameMy = "name";
+const nameIs = "is";
+const fullName = `${myName} ${nameMy} ${nameIs} Viktor`;
+console.log(fullName)
+
+// завдання 4
+
+const userName = "Бобік";
+const payment = 300;
+const paymentName = `Дякуємо, ${userName}! До сплати ${payment} гривень`;
+alert(paymentName)
